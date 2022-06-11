27b9a5952354b69a3ad5e19159fc956281c36a13
OndrejMus <77000568+OndrejMus@users.noreply.github.com>
2022-06-11 15:54:56 +0200

    Create EncodeDecodeURL.js

---
+++ EncodeDecodeURL.js
@@ -0,0 +1,9 @@
+// https://www.w3schools.com/jsref/jsref_encodeuri.asp
+let uri = "my test.asp?name=ståle&car=saab";
+let encoded = encodeURI(uri);
+
+
+// https://www.w3schools.com/jsref/jsref_decodeuri.asp
+let uri = "my test.asp?name=ståle&car=saab";
+let encoded = encodeURI(uri);
+let decoded = decodeURI(encoded);
