19e61103d31df2b468d40b98d87ede6cfa647bc8
OndrejMus <77000568+OndrejMus@users.noreply.github.com>
2022-06-15 14:29:29 +0200

    Create DBIndexFragmentation.sql

---
+++ SQL/DBIndexFragmentation.sql
@@ -0,0 +1,14 @@
+-- Returns all indexes from selected database with average fragmentation and page count
+-- Ordered by average fragmentation, descending
+SELECT dbschemas.[name] as 'Schema',
+dbtables.[name] as 'Table',
+dbindexes.[name] as 'Index',
+indexstats.avg_fragmentation_in_percent,
+indexstats.page_count
+FROM sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL, NULL, NULL) AS indexstats
+INNER JOIN sys.tables dbtables on dbtables.[object_id] = indexstats.[object_id]
+INNER JOIN sys.schemas dbschemas on dbtables.[schema_id] = dbschemas.[schema_id]
+INNER JOIN sys.indexes AS dbindexes ON dbindexes.[object_id] = indexstats.[object_id]
+AND indexstats.index_id = dbindexes.index_id
+WHERE indexstats.database_id = DB_ID()
+ORDER BY indexstats.avg_fragmentation_in_percent desc
