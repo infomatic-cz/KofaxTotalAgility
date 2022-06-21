b9e45312fe8f8b035ebfbee2be728366103bd755
OndrejMus <77000568+OndrejMus@users.noreply.github.com>
2022-06-21 17:02:09 +0200

    Commet spelling

--- SQL/UserActionsAudit.sql
+++ SQL/UserActionsAudit.sql
@@ -1,4 +1,4 @@
--- Returns last 1000 audit entries related to user login or logoff
+-- Returns last 1000 audit entries
 -- Can be filtered to specific user by populating @UserName variable with partial or full username
 DECLARE @UserName nvarchar(50) = ''
 SELECT TOP 1000 AUDIT_TIME,
@@ -8,9 +8,4 @@ SELECT TOP 1000 AUDIT_TIME,
 FROM dbo.AUDIT_LOG a
     INNER JOIN dbo.NT_RESOURCE r ON a.RESOURCE_ID = r.RESOURCE_ID
     AND r.NT_NAME LIKE '%' + isnull(nullif(@UserName, ''), r.NT_NAME) + '%'
-WHERE AUDIT_TYPE IN (
-        SELECT AUDIT_TYPE
-        FROM dbo.AUDIT_TYPE
-        WHERE AUDIT_CATEGORY = 8
-    )
 ORDER BY AUDIT_TIME desc
