c9ed9aae535caa695939b20a64c0e0b01b88876c
OndrejMus <77000568+OndrejMus@users.noreply.github.com>
2022-08-04 14:33:08 +0200

    Added script tester project

---
+++ C#/KTA-ScriptNoteTester/KTA-ScriptNoteTester/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+// Common .Net features
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+// KTA 
+using TotalAgility.Sdk;
+using Agility.Sdk.Model;
+using Agility.Server.Scripting.ScriptAssembly;
+// Database
+using System.Data;
+using System.Data.SqlClient;
+
+namespace KTA_ScriptNoteTester
+{
+    class Class1
+    {
+        [StartMethodAttribute()]
+        public void Method1(ScriptParameters sp)
+        {
+            JobService jobService = new JobService();
+
+            string sessionId = sp.InputVariables["SPP_SYSTEM_SESSION_ID"].ToString();
+            //string documentId = sp.InputVariables["InputProcessVariableName"].ToString();
+
+            Agility.Sdk.Model.Jobs.JobFilter4 jobFilter = new Agility.Sdk.Model.Jobs.JobFilter4();
+
+            jobFilter.MaxNumberToRetrieve = 50;
+            jobFilter.JobStatusFilter = 1;
+
+            Agility.Sdk.Model.Jobs.JobList jobList = jobService.GetJobs4(sessionId, jobFilter);
+
+        }
+    }
+}
---
+++ C#/KTA-ScriptNoteTester/KTA-ScriptNoteTester/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agility.Server.Scripting;
+using Agility.Server.Scripting.ScriptAssembly;
+
+namespace KTA_ScriptNoteTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ParameterCollection input = new ParameterCollection();
+            input.Add("SPP_SYSTEM_SESSION_ID", "68BB738BCA4E364593A2222D27750691");
+            //input.Add("JOBID", "C3C69AE7B324409D8F3DB967F4A41FF9");
+            //input.Add("FOLDERID", "9843a23c-fa5f-44f7-a817-ab1f00b8b3d7");
+
+            ParameterCollection output = new ParameterCollection();
+            output.Add("JobPropertiesXML", null);
+            output.Add("JobVariablesXML", null);
+
+            ScriptParameters sp = new ScriptParameters(input, output);
+
+            Class1 tester = new Class1();
+            tester.Method1(sp);
+
+        }
+    }
+}
