f146e03c81e3eb4e10436c01aebe458a5b8468c7
OndrejMus <77000568+OndrejMus@users.noreply.github.com>
2022-08-26 12:06:07 +0200

    Update GetAndTerminateServerConnection.sql

--- SQL/ServerAndDatabase/GetAndTerminateServerConnection.sql
+++ SQL/ServerAndDatabase/GetAndTerminateServerConnection.sql
@@ -1,6 +1,7 @@
 -- In case of DB operations that require no active connections it is possible to list and terminate connections
 
--- List connections
+-- List connections, for more details use sp_who2
+-- Require VIEW SERVER STATE permission
 exec sp_who
 
 -- Kill specific connetion by id (xxx = spid from previous result)
