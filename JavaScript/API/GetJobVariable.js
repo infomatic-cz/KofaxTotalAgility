cfad62698230bdae5f05275a47a7a4fbb8a4750d
OndrejMus <77000568+OndrejMus@users.noreply.github.com>
2022-09-22 10:46:41 +0200

    script for fast job variable download

---
+++ JavaScript/API/GetJobVariable.js
@@ -0,0 +1,47 @@
+// Set jobid and variable id, you need to be logged in KTA Workspace
+var jobid = "D14D6866230511ED8136005056940698";
+var variableid = "FOLDER";
+
+// 
+var GetJobVariableValueStruct = function () {
+var struct = {
+    "sessionId": "",
+    "jobIdentity":{
+        "Id":""
+    },
+    "variableIdentity":{
+        "Id":""
+    }  
+};
+
+return struct;
+};
+
+var getJobVariable = new GetJobVariableValueStruct();
+
+getJobVariable.sessionId = JSON.parse(sessionStorage.getItem("SESSION_ID")).value;
+getJobVariable.jobIdentity.Id = jobid;
+getJobVariable.variableIdentity.Id = variableid;
+
+//$("#Request").html("<BR/><B>Request:</B><BR/>" + JSON.stringify(createJobJSON));
+
+// Calling method CreateJob
+$.ajax({
+type: "POST",
+async: "false",
+url: window.location.origin+"/TotalAgility/Services/Sdk/JobService.svc/json/GetJobVariableValue",
+data: JSON.stringify(getJobVariable),
+contentType: "application/json; charset=utf-8",
+crossDomain: true,
+dataType: "json",
+success: function (result) {
+    //$("#Result").html("<BR/><B>Response: </B><BR/>" + JSON.stringify(result.d));
+    //console.log(JSON.stringify(result.d));
+    console.log(result.d.Value);
+},
+error: function (jqXHR, textStatus, errorThrown) {
+    //alert("***Error***\n");
+    console.log(textStatus);
+    console.log(errorThrown);
+}
+});
